64e194e0f2ba47faf197aac2f731582c0e5a3415
Theo Browne <me@t3.gg>
2022-07-03 00:18:43 -0700

    Lambda cache edge options

---
+++ pages/lambda.tsx
@@ -0,0 +1,47 @@
+import type { NextPage } from "next";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"
+        style={{ width: "64px", marginTop: "2rem" }}
+        alt="svelte logo"
+      />
+      <h1>Theo&apos;s Crappy Benchmark (NextJS On Vercel Edge)</h1>
+      <h2>
+        Next hates my inline script so times will show up ~1000ms after
+        they&apos;re calculated
+      </h2>
+
+      <h2>
+        <span>
+          Full request to render time (according to Theo):{" "}
+          <span id="overrideme" />
+          ms
+        </span>
+      </h2>
+      <script>
+        {`
+      const currentTime = new Date(); // round trip time
+      const fullTime = currentTime - window.performance.timing.requestStart;
+      console.log(\`THEO REPORTS\`, fullTime);
+      setTimeout(function() {
+        document.getElementById(\`overrideme\`).innerHTML = fullTime;
+      }, 1000);
+        `}
+      </script>
+    </div>
+  );
+};
+
+export default Home;
+
+export const getServerSideProps = () => {
+  return { props: { message: new Date().toString() } };
+};
+
+export const config = {
+  runtime: "nodejs",
+};
