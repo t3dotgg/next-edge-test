d44c7189d8279b7de0fbc40b9e1e29ad1aeb2ec3
Theo Browne <me@t3.gg>
2022-07-03 00:41:13 -0700

    reuse core component

--- pages/edge.tsx
+++ pages/edge.tsx
@@ -1,33 +1,8 @@
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
+import { Benchmarker } from "../components/benchmarker";
 
 const Home: NextPage = () => {
-  return (
-    <div className={styles.container}>
-      <img
-        src="https://upload.wikimedia.org/wikipedia/commons/8/8e/Nextjs-logo.svg"
-        style={{ width: "64px", marginTop: "2rem" }}
-        alt="svelte logo"
-      />
-      <h1>Theo&apos;s Crappy Benchmark (NextJS On Vercel Edge)</h1>
-
-      <h2>
-        <span>
-          Full request to render time (according to Theo):{" "}
-          <span id="overrideme" />
-          ms
-        </span>
-      </h2>
-      <script>
-        {`
-      const currentTime = new Date(); // round trip time
-      const fullTime = currentTime - window.performance.timing.requestStart;
-      console.log(\`THEO REPORTS\`, fullTime);
-      document.getElementById(\`overrideme\`).innerHTML = fullTime;
-        `}
-      </script>
-    </div>
-  );
+  return <Benchmarker>Edge</Benchmarker>;
 };
 
 export default Home;
